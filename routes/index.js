4962c85f707d707a65a7593bae207c839faf57d2
Jeremy Pridemore <jeremy.pridemore@gmail.com>
2014-06-30 21:27:13 -0600

    Show domain a user came from.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var environment = express().get('env');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express', env: environment});
+  res.render('index', { title: 'Express', env: environment, domain: req.get('host')});
 });
 
 module.exports = router;
