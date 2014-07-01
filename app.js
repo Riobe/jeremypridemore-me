379a1e86b8aba2992d1f1971f8e98d43c75fe812
Jeremy Pridemore <jeremy.pridemore@gmail.com>
2014-06-30 20:48:41 -0600

    Inject database during pipeline

--- app.js
+++ app.js
@@ -4,19 +4,28 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongodb = require('mongodb');
 
 var app = express();
 
-app.set('mongodb-connection-string',
-        app.get('env') === 'production'
-            ? 'mongodb://riobe:jpridemore-Mongo@ds059898.mongolab.com:59898/test'
-            : 'mongodb://localhost/test');
+// mongodb://riobe:jpridemore-Mongo@ds059898.mongolab.com:59898/heroku_app26822700
+// mongo ds059898.mongolab.com:59898/heroku_app26822700 -u riobe -p jpridemore-Mongo
+var database = {};
+mongodb.MongoClient.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/heroku_app26822700', function(err, db) {
+    if (err) throw err;
+
+    database.users = db.collection('users');
+});
+
+app.use(function(req, res, next) {
+    req.db = database;
+
+    next();
+})
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
-// mongodb://riobe:jpridemore-Mongo@ds059898.mongolab.com:59898/heroku_app26822700
-// mongo ds059898.mongolab.com:59898/heroku_app26822700 -u riobe -p jpridemore-Mongo
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
--- routes/users.js
+++ routes/users.js
@@ -1,24 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var mongodb = require('mongodb');
-
-var app = express();
-var mongoDbConnectionString = app.get('mongodb-connection-string')
-
-var server = new mongodb.Server(mongoDbConnectionString, 27017, {auto_reconnect: true});
-var testCollection = {};
-db = new mongodb.Db('test', server);
-
-db.open(function(err, db) {
-    if(!err) {
-        console.log("Connected to 'test' database");
-        testCollection = db.collection('users');
-    }
-});
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-    testCollection.find({}, {}, function(err, docs) {
+    req.db.users.find().toArray(function(err, docs) {
         res.render('users', {users: docs});
     });
 });
@@ -28,16 +13,16 @@ router.get('/:id', function(req, res) {
 });
 
 router.post('/', function(req, res) {
-    testCollection.insert(req.body, function(err, docs) {
+    req.db.users.insert(req.body, function(err, docs) {
         if (err) {
             // If it failed, return error
             res.send("There was a problem adding the information to the database.");
             return;
         }
 
-        res.location("/");
+        res.location("/users");
         // And forward to success page
-        res.redirect("/");
+        res.redirect("/users");
     });
 });
 
