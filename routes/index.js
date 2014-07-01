c9cc5e57d608b4a118644c5863d2334dffdd1465
Jeremy Pridemore <jeremy.pridemore@gmail.com>
2014-06-30 19:34:03 -0600

    Removed an unnecessary call.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var app = express();
+var environment = express().get('env');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express', env: app.get('env') });
+  res.render('index', { title: 'Express', env: environment});
 });
 
 module.exports = router;
