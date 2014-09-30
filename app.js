5d8aefc4d7d23e9b2db319c9089f45594e90efd5
riobe <jeremy.pridemore@gmail.com>
2014-09-29 22:24:59 -0600

    Adding resume-builder experiment to the website, nonfunctional.

--- app.js
+++ app.js
@@ -85,6 +85,7 @@ app.use(function(req, res, next) {
 
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
+app.use('/resume-builder', require('./routes/resume-builder'));
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
