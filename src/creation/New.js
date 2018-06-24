de80636f67e1d491821c5d6b1ad483dffd87c37c
riobe <jeremy.pridemore@gmail.com>
2018-06-24 15:01:19 -0600

    Default attributes to 1, and add initial abilities bindings.

--- src/creation/New.js
+++ src/creation/New.js
@@ -1,24 +1,58 @@
 import React, { Component } from 'react';
+
+import { titleCase } from '../util';
+
 import AttributeRow from './AttributeRow';
+import DotSet5 from './DotSet5';
+
+const debug = require('debug')('jeremypridemore-me:App');
 
 export default class New extends Component {
   state = {
     attributes: {
       physical: {
-        strength: 3,
-        dexterity: 2,
+        strength: 1,
+        dexterity: 1,
         stamina: 1
       },
       social: {
-        charisma: 2,
-        manipulation: 2,
-        appearance: 2
+        charisma: 1,
+        manipulation: 1,
+        appearance: 1
       },
       mental: {
-        perception: 2,
-        intelligence: 2,
-        wits: 2
+        perception: 1,
+        intelligence: 1,
+        wits: 1
       }
+    },
+    abilities: {
+      archery: 0,
+      athletics: 0,
+      awareness: 0,
+      brawl: 0,
+      bureaucracy: 0,
+      craft: 0,
+      dodge: 0,
+      integrity: 0,
+      investigation: 0,
+      larceny: 0,
+      linguistics: 0,
+      lore: 0,
+      martialArts: 0,
+      medicine: 0,
+      melee: 0,
+      occult: 0,
+      performance: 0,
+      presence: 0,
+      resistance: 0,
+      ride: 0,
+      sail: 0,
+      socialize: 0,
+      stealth: 0,
+      survival: 0,
+      thrown: 0,
+      war: 0
     }
   }
 
@@ -50,7 +84,7 @@ export default class New extends Component {
           <h3>Attributes</h3>
         </div>
 
-        <div className="shadow-lg">
+        <div id="attributes" className="shadow-lg">
           {Object.keys(this.state.attributes).map(type => (
             <AttributeRow
               key={type}
@@ -58,6 +92,31 @@ export default class New extends Component {
               attributeType={this.state.attributes[type]}
               onValueChanged={({attribute, newValue}) => this.handleAttributeChange(type, attribute, newValue)}
             />
+          ))}
+        </div>
+
+        <div id="abilities" className="shadow-lg">
+          {Object.keys(this.state.abilities).map(ability => (
+            <div className="ability" key={ability}>
+              <div className="ability-header">{titleCase(ability)}</div>
+              <DotSet5
+                className="ability-value"
+                value={this.state.abilities[ability]}
+                onValueChanged={value => {
+                  debug(`${ability} set to ${value}`);
+                  const newState = {
+                    abilities: {
+                      ...this.state.abilities,
+                      [ability]: value
+                    }
+                  };
+                  debug('Setting state to:', newState);
+
+                  this.setState(newState);
+                }}
+                clearable={true}
+              />
+            </div>
           ))}
         </div>
 
